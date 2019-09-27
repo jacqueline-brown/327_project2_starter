eed2e800b88858d3fcf57fb258a4e0cdb18d5c66
jackie <jacqueline.brown.18@cnu.edu>
2019-09-26 23:35:20 -0400

    finished project

--- src/array_functions.cpp
+++ src/array_functions.cpp
@@ -28,37 +28,30 @@ struct tracker {
 	int numberOfOccurances;
 };
 //TODO add a global array of entry structs (global to this file)
-tracker arrayOfStructs[constants::MAX_WORDS];
+tracker arrayOfStructs[MAX_WORDS];
 //TODO add variable to keep track of next available slot in array
-int nextSlot;
+int nextSlot=0;
 //TODO define all functions in header file
 void clearArray() {
-
+	nextSlot = 0;
 }
 
 int getArraySize() {
-	int count = 0;
-	/*for(int i = 0; unsigned(i) < sizeof(arrayOfStructs); i++){
-		if(!end(arrayOfStructs))
-				count += 1;
-	}*/
-	while(!(end(arrayOfStructs)))
-		count += 1;
-	return count;
+	return nextSlot;
 }
 
 std::string getArrayWordAt(int i){
-	return "";
+	return arrayOfStructs[i].word;
 }
 
 int getArrayWord_NumbOccur_At(int i) {
-	return 0;
+	return arrayOfStructs[i].numberOfOccurances;
 }
 
 bool processFile(std::fstream &myfstream) {
 	string line;
-	if (!(myfstream.is_open()))
-		return false;
+	if (!(myfstream.is_open())){
+		return false; }
 	while (!(myfstream.eof())){
 		getline(myfstream, line);
 		processLine(line);
@@ -67,35 +60,123 @@ bool processFile(std::fstream &myfstream) {
 }
 
 void processLine(std::string &myString){
-	strip_unwanted_chars(myString);
 	stringstream ss(myString);
 	string token;
-	while(getline(ss,token,constants::CHAR_TO_SEARCH_FOR)) {
+	while(getline(ss,token,CHAR_TO_SEARCH_FOR)) {
 		processToken(token);
 	}
 }
 
 void processToken(std::string &token) {
+	strip_unwanted_chars(token);
+	if(!(strip_unwanted_chars(token))){
+			return;
+	}
+	for(int i = 0; i < getArraySize(); i++){
+		string word1 = arrayOfStructs[i].word;
+		toUpper(word1);
+		string tempToken = token;
+		toUpper(tempToken);
+
+		if(word1 == tempToken) {
+			arrayOfStructs[i].numberOfOccurances += 1;
+			return;
+		}
+	}
+
+	if(token.length() != 0){
+		arrayOfStructs[nextSlot] = tracker();
+		arrayOfStructs[nextSlot].numberOfOccurances = 1;
+		arrayOfStructs[nextSlot].word = token;
+		nextSlot++;
+	}
+
 
 }
 
 bool openFile(std::fstream& myfile, const std::string& myFileName, std::ios_base::openmode mode) {
 	myfile.open(myFileName.c_str());
-	if (myfile.is_open()) {
-		return true;
-	}
-	return false;
+	return myfile.is_open();
 }
 
 void closeFile(std::fstream& myfile){
-	myfile.close();
+	if(myfile.is_open()==true){
+		myfile.close();
+}
 }
 
 int writeArraytoFile(const std::string &outputfilename){
-	return 0;
+	ofstream wordcounter;
+	wordcounter.open(outputfilename.c_str());
+	if(!(wordcounter.is_open()))
+		return FAIL_FILE_DID_NOT_OPEN;
+	if(getArraySize() == 0)
+		return FAIL_NO_ARRAY_DATA;
+
+	for(int i = 0; i < getArraySize(); i++){
+		wordcounter << arrayOfStructs[i].word << " " << arrayOfStructs[i].numberOfOccurances << endl;
+	}
+	wordcounter.close();
+
+	return SUCCESS;
 }
 
 void sortArray(constants::sortOrder so){
+	switch (so) {
+	case constants::NONE:
+		break;
+	case constants::ASCENDING:
+	{
+		tracker temp;
+		for(int i = 0; i < getArraySize()-1; i++) {
+			for(int j = i+1; j < getArraySize(); j++){
+				string word1 = arrayOfStructs[i].word;
+				string word2 = arrayOfStructs[j].word;
+				toUpper(word1);
+				toUpper(word2);
+				if(word1 > word2) {
+					temp = arrayOfStructs[i];
+					arrayOfStructs[i] = arrayOfStructs[j];
+					arrayOfStructs[j] = temp;
+				}
+			}
+		}
+		break;
+	}
+	case constants::DESCENDING:
+	{
+		tracker temp ;
+			for(int i = 0; i < getArraySize()-1; i++) {
+				for(int j = i + 1; j < getArraySize(); j++){
+					string word1 = arrayOfStructs[i].word;
+					string word2 = arrayOfStructs[j].word;
+					toUpper(word1);
+					toUpper(word2);
+					if(word1 < word2) {
+						temp = arrayOfStructs[i];
+						arrayOfStructs[i] = arrayOfStructs[j];
+						arrayOfStructs[j] = temp;
+					}
+				}
 
+			}
+		break;
+	}
+	case constants::NUMBER_OCCURRENCES:
+	{
+		for(int i = 0; i < getArraySize(); i++){
+			for(int j = 0; j < getArraySize(); j++) {
+				int num1 = arrayOfStructs[i].numberOfOccurances;
+				int num2 = arrayOfStructs[j].numberOfOccurances;
+				if(num1 > num2) {
+					tracker temp = arrayOfStructs[i];
+					arrayOfStructs[i] = arrayOfStructs[j];
+					arrayOfStructs[j] = temp;
+				}
+			}
+		}
+		break;
+	}
+}
 }
 //TODO look in utilities.h for useful functions, particularly strip_unwanted_chars!
